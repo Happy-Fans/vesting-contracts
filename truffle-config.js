157e37bab8f37375d5f03c612fe1486d7ecb6b44
Enry679 <91887586+Enry679@users.noreply.github.com>
2021-10-12 18:17:55 +0200

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
 
     testnet: {
-      provider: () => new HDWalletProvider(process.env.MNEMONIC, '\'https://data-seed-prebsc-1-s1.binance.org:8545\''),
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, 'https://data-seed-prebsc-1-s1.binance.org:8545'),
       network_id: 97,
       confirmations: 5,
       networkCheckTimeout: 1000000,
